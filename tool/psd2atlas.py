287920adf3f7b82fea830ce4e5830cc687f5e2c4
hundoy <zoharwolf@qq.com>
2017-09-27 16:20:40 +0800

    finish psd2atlas.py

--- tool/psd2atlas.py
+++ tool/psd2atlas.py
@@ -2,22 +2,46 @@
 # -*- coding: utf-8 -*-
 from PIL import Image
 from psd_tools import PSDImage
-import PIL
+import sys
+import json
+import os
 
 
-def merge(frect,face):
+def merge(frect, face):
     face_img = face.as_PIL()
     box = frect.bbox
     fbox = face.bbox
-    img = Image.new("RGBA", (box.width,box.height), (255,255,255,0))
-    new_fi = face_img.crop((0,0,fbox.width,fbox.height))
-    img.paste(new_fi, (fbox.x1-box.x1, fbox.y1-box.y1, fbox.x1-box.x1+fbox.width, fbox.y1-box.y1+fbox.height))
+    img = Image.new("RGBA", (box.width, box.height), (255, 255, 255, 0))
+    new_fi = face_img.crop((0, 0, fbox.width, fbox.height))
+    img.paste(new_fi,
+              (fbox.x1 - box.x1, fbox.y1 - box.y1, fbox.x1 - box.x1 + fbox.width, fbox.y1 - box.y1 + fbox.height))
     return img
 
 
-print "start"
-psd = PSDImage.load("psdexample.psd")
+print "start psd2atlas"
 
+# check arguments
+if len(sys.argv)!=2:
+    print "[ERROR]Please pass psd file name as param! Such as: ./psd2atlas.py psdexample"
+    exit(0)
+
+filename = sys.argv[1]
+psd = PSDImage.load(filename+".psd")
+dir_name = filename+"/"
+lh_info = {"faceRectX":0, "faceRectY":0}
+
+
+if not os.path.exists(filename):
+    os.makedirs(filename)
+else:
+    oldfiles = os.listdir(filename)
+    for oldfile in oldfiles:
+        os.remove(dir_name+oldfile)
+
+# create an image with the whole psd size as the base_lh
+lh_img = Image.new("RGBA", (psd.bbox.width, psd.bbox.height), (255, 255, 255, 0))
+
+# get the face_rect from face group.
 face_rect = None
 for group in psd.layers:
     if group.name == 'face':
@@ -27,10 +51,13 @@ for group in psd.layers:
                 break
         break
 
+
 if face_rect is None:
-    print "there is not face_rect layer in face group!"
+    print "[ERROR]There is not face_rect layer in face group!"
     exit(0)
 
+
+# iterate all the layers to get lh_base and face_xxx
 for layer in psd.layers:
     name = layer.name
     if name == 'face':
@@ -40,10 +67,17 @@ for layer in psd.layers:
 
             print face.name
             img = merge(face_rect, face)
-            img.save(face.name + ".png");
+            img.save("%slh_%s_%s.png" % (dir_name, filename, face.name))
     elif name == 'lh_base':
         print name
-        layer_image = layer.as_PIL()
-        layer_image.save(name + ".png");
+        lh_box = layer.bbox
+        lh_info["faceRectX"] = face_rect.bbox.x1
+        lh_info["faceRectY"] = face_rect.bbox.y1
+        lh_layer_image = layer.as_PIL()
+        lh_img.paste(lh_layer_image, (lh_box.x1, lh_box.y1, lh_box.x2, lh_box.y2))
+        lh_img.save("%slh_%s.png" % (dir_name, filename))
+
+        with open("%slh_%s_info.json" % (dir_name, filename), 'w') as of:
+            of.write(json.dumps(lh_info))
 
 print "end"
